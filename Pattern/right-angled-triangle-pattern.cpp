d8b5e73b8fb4db8abd2fc92fc37d3bd5e04d6761
hukes-chandra <hukeshchandrawork@gmail.com>
2025-05-30 10:33:22 +0530

    forst commit

---
+++ Pattern/right-angled-triangle-pattern.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+void astericks1(int n){
+        for(int i = 0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void astericks2(int n){
+        for(int i = 0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void number1(int n){
+        for(int i = 1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void number2(int n){
+        for(int i = 1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void number3(int n){
+        for(int i = 0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout<<n-j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void number4(int n){
+        for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            if((i-j)%2==0){
+                cout<<1<<" ";
+            }else{
+                cout<<0<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}
+
+void number5(int n){
+    int num =1;
+        for(int i = 1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
+
+void alphabet1(int n){
+        for(int i = 1;i<=n;i++){
+        for(char ch = 'A';ch<='A'+i;ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void alphabet2(int n){  //unsolved 15
+        for(int i = 0;i<n;i++){
+        for(char ch='A';ch>'A'+i;ch--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    alphabet1(n);
+    return 0;
+}
